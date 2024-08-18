02ef6df186d92cc2e52f269cc8d18961e56e9bf5
maheeen-khan <maheeenkhan4@gmail.com>
2024-08-19 00:22:45 +0500

    changes

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 function mainPage(){
-    // console.log('hi');
+    
     console.log(document.getElementById('board-name').value);
+
     var boardName = document.getElementById('board-name').value;
+    // document.getElementById('sec-nav').innerHTML += `hi`;
+
+        // Save the board name to localStorage
+        localStorage.setItem('boardName', boardName);
+
+        // Navigate to the second page
+        window.location.href = './main.html';
+
+    // console.log(document.querySelector('#sec-nav'))
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Retrieve the board name from localStorage
+    var boardName = localStorage.getItem('boardName');
+
+    // If a board name exists, display it in the desired element
+    if (boardName) {
+        document.getElementById('sec-nav').innerHTML += ` ${boardName}`;
+    }
+});
 
 
 function enter(event){
