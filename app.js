d590628e8668d9ff646e70304530eab7fda10a08
maheeen-khan <maheeenkhan4@gmail.com>
2024-08-20 20:09:34 +0500

    changes

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 function mainPage(){
-    
-    // console.log(document.getElementById('board-name').value);
-
     var boardName = document.getElementById('board-name').value;
 
-        // Save the board name to localStorage
-        localStorage.setItem('boardName', boardName);
+    // Save the board name to localStorage
+    localStorage.setItem('boardName', boardName);
 
+    // Clear the input value
+    document.getElementById('board-name').value = '';
 
-        document.getElementById('board-name').value = '';
-
-        
-        // Navigate to the second page
-        window.location.href = './main.html';
-    
-        
-
+    // Navigate to the second page
+    window.location.href = './main.html';
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -27,32 +20,48 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('sec-nav').innerHTML += `<h5> ${boardName}</h5>`;
     }
 
-    
     var lists = document.getElementById('lists');
-    console.log(lists);
 
-    var newList = this.createElement('div');
-
+    var newList = document.createElement('div');
     newList.setAttribute('class', 'list');
     newList.classList.add('col-lg-4', 'col-md-4', 'col-sm-12');
 
-    newList.innerHTML = `<button class="btn add-list"> <span class="material-symbols-outlined pe-2"> add </span> Add list</button>`;
+    // Create the "Add list" button
+    const addListButton = document.createElement('button');
+    addListButton.className = 'btn add-list';
+    addListButton.innerHTML = '<span class="material-symbols-outlined pe-2"> add </span> Add list';
+    
+    // Set up the click event to show the input field
+    addListButton.onclick = function() {
+        enterListName();
+    };
+    
+    // Append the "Add list" button to the new list
+    newList.appendChild(addListButton);
 
+    // Define the function to show the input field
+    function enterListName() {
+        // Create and show the input field for the list name
+        const inputField = document.createElement('input');
+        inputField.type = 'text';
+        inputField.name = 'todo';
+        inputField.id = 'todo';
+        inputField.className = 'my-3 p-1';
+        inputField.placeholder = 'Enter list name';
 
-    // newList.innerHTML += `<br><input type="text" name="todo" id="todo" class="my-3 p-1" placeholder="Kickoff meeting">`;
-    // newList.innerHTML += `<br><button class="btn"> <span class="material-symbols-outlined pe-2"> add </span> Add a card</button>`
+        // Add the input field after the "Add list" button
+        newList.appendChild(inputField);
 
-    console.log(newList);
+        // Disable the "Add list" button after it's clicked to prevent multiple inputs
+        addListButton.disabled = true;
+    }
 
+    // Append the new list to the lists container
     lists.appendChild(newList);
-
 });
-
 
 function enter(event){
     if(event.keyCode === 13){
         mainPage();
     }
 }
-
-// second page
